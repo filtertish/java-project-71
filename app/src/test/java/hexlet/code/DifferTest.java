dbfe9d746b05826ff7e10cc995dc380de80b7f51
fugu <ffqwerty3@gmail.com>
2024-05-28 19:07:20 +0300

    add test

---
+++ app/src/test/java/hexlet/code/DifferTest.java
@@ -0,0 +1,34 @@
+package hexlet.code;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+public class DifferTest {
+    @Test
+    public void differTestAdd() {
+        String result = Differ.generate("src/test/resources/base.json", "src/test/resources/add_to_base.json");
+        String actualResult = """
+            {
+                admin: false
+                age: 30
+                email: hello_world@gmail.com
+                name: Bob
+              + newLine: i was just added
+            }""";
+        Assertions.assertEquals(result, actualResult);
+    }
+
+    @Test
+    public void differTestRemove() {
+        String result = Differ.generate("src/test/resources/base.json", "src/test/resources/remove_from_base.json");
+        String actualResult = """
+            {
+              - admin: false
+                age: 30
+              - email: hello_world@gmail.com
+                name: Bob
+            }""";
+
+        Assertions.assertEquals(result, actualResult);
+    }
+}
