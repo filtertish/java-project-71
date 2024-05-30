b9d9840024770b304939853454f0db5889c50ae4
fugu <ffqwerty3@gmail.com>
2024-05-30 19:59:22 +0300

    add tests for files with nested fields

--- app/src/test/java/hexlet/code/DifferTest.java
+++ app/src/test/java/hexlet/code/DifferTest.java
@@ -6,57 +6,66 @@ import org.junit.jupiter.api.Test;
 
 public class DifferTest {
     @Test
-    public void differTestJSONAdd() {
-        String result = Differ.generate("src/test/resources/base.json", "src/test/resources/add_to_base.json");
+    public void differTestJSON() {
+        String result = Differ.generate("src/test/resources/base.json", "src/test/resources/modified.json");
         String actualResult = """
-            {
-                admin: false
-                age: 30
-                email: hello_world@gmail.com
-                name: Bob
-              + newLine: i was just added
-            }""";
+                                 {
+                                     chars1: [a, b, c]
+                                   - chars2: [d, e, f]
+                                   + chars2: false
+                                   - checked: false
+                                   + checked: true
+                                   - default: null
+                                   + default: [value1, value2]
+                                   - id: 45
+                                   + id: null
+                                   - key1: value1
+                                   + key2: value2
+                                     numbers1: [1, 2, 3, 4]
+                                   - numbers2: [2, 3, 4, 5]
+                                   + numbers2: [22, 33, 44, 55]
+                                   - numbers3: [3, 4, 5]
+                                   + numbers4: [4, 5, 6]
+                                   + obj1: {nestedKey=value, isNested=true}
+                                   - setting1: Some value
+                                   + setting1: Another value
+                                   - setting2: 200
+                                   + setting2: 300
+                                   - setting3: true
+                                   + setting3: none
+                                 }""";
         Assertions.assertEquals(result, actualResult);
     }
 
     @Test
-    public void differTestJSONRemove() {
-        String result = Differ.generate("src/test/resources/base.json", "src/test/resources/remove_from_base.json");
+    public void differTestYAML() {
+        String result = Differ.generate("src/test/resources/base.yaml", "src/test/resources/modified.yaml");
         String actualResult = """
-            {
-              - admin: false
-                age: 30
-              - email: hello_world@gmail.com
-                name: Bob
-            }""";
-
-        Assertions.assertEquals(result, actualResult);
-    }
-
-    @Test
-    public void differTestYAMLAdd() {
-        String result = Differ.generate("src/test/resources/base.yml", "src/test/resources/add_to_base.yml");
-        String actualResult = """
-            {
-                admin: false
-                age: 30
-                email: hello_world@gmail.com
-                name: Bob
-              + newLine: i was just added
-            }""";
-        Assertions.assertEquals(result, actualResult);
-    }
-
-    @Test
-    public void differTestYAMLRemove() {
-        String result = Differ.generate("src/test/resources/base.yml", "src/test/resources/remove_from_base.yml");
-        String actualResult = """
-            {
-              - admin: false
-                age: 30
-              - email: hello_world@gmail.com
-                name: Bob
-            }""";
+                                 {
+                                     chars1: [a, b, c]
+                                   - chars2: [d, e, f]
+                                   + chars2: false
+                                   - checked: false
+                                   + checked: true
+                                   - default: null
+                                   + default: [value1, value2]
+                                   - id: 45
+                                   + id: null
+                                   - key1: value1
+                                   + key2: value2
+                                     numbers1: [1, 2, 3, 4]
+                                   - numbers2: [2, 3, 4, 5]
+                                   + numbers2: [22, 33, 44, 55]
+                                   - numbers3: [3, 4, 5]
+                                   + numbers4: [4, 5, 6]
+                                   + obj1: {nestedKey=value, isNested=true}
+                                   - setting1: Some value
+                                   + setting1: Another value
+                                   - setting2: 200
+                                   + setting2: 300
+                                   - setting3: true
+                                   + setting3: none
+                                 }""";
 
         Assertions.assertEquals(result, actualResult);
     }
