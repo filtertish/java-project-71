a3d663f7bb09965cf8621566e64239d4cb5e124c
fugu <ffqwerty3@gmail.com>
2024-05-30 13:09:14 +0300

    add yaml parser tests

--- app/src/test/java/hexlet/code/DifferTest.java
+++ app/src/test/java/hexlet/code/DifferTest.java
@@ -6,7 +6,7 @@ import org.junit.jupiter.api.Test;
 
 public class DifferTest {
     @Test
-    public void differTestAdd() {
+    public void differTestJSONAdd() {
         String result = Differ.generate("src/test/resources/base.json", "src/test/resources/add_to_base.json");
         String actualResult = """
             {
@@ -20,8 +20,36 @@ public class DifferTest {
     }
 
     @Test
-    public void differTestRemove() {
+    public void differTestJSONRemove() {
         String result = Differ.generate("src/test/resources/base.json", "src/test/resources/remove_from_base.json");
+        String actualResult = """
+            {
+              - admin: false
+                age: 30
+              - email: hello_world@gmail.com
+                name: Bob
+            }""";
+
+        Assertions.assertEquals(result, actualResult);
+    }
+
+    @Test
+    public void differTestYAMLAdd() {
+        String result = Differ.generate("src/test/resources/base.yml", "src/test/resources/add_to_base.yml");
+        String actualResult = """
+            {
+                admin: false
+                age: 30
+                email: hello_world@gmail.com
+                name: Bob
+              + newLine: i was just added
+            }""";
+        Assertions.assertEquals(result, actualResult);
+    }
+
+    @Test
+    public void differTestYAMLRemove() {
+        String result = Differ.generate("src/test/resources/base.yml", "src/test/resources/remove_from_base.yml");
         String actualResult = """
             {
               - admin: false
