8fc81ca54d7e9621a0e7304b19db9ae3189dca01
fugu <ffqwerty3@gmail.com>
2024-07-07 07:20:52 +0300

    add JSONAssert

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -19,6 +19,7 @@ repositories {
 dependencies {
     testImplementation(platform("org.junit:junit-bom:5.11.0-M1"))
     testImplementation("org.junit.jupiter:junit-jupiter")
+    testImplementation("org.skyscreamer:jsonassert:1.5.3")
     implementation("info.picocli:picocli:4.7.6")
     annotationProcessor("info.picocli:picocli-codegen:4.7.6")
     implementation("com.fasterxml.jackson.core:jackson-databind:2.17.1")
