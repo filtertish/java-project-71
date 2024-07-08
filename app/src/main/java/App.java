5dff1c32efd308f86d9bdb835222a0e900f88a24
fugu <ffqwerty3@gmail.com>
2024-07-08 14:06:07 +0300

    fix picocli class

--- app/src/main/java/App.java
+++ app/src/main/java/App.java
@@ -1,30 +1,34 @@
+import hexlet.code.Differ;
 import picocli.CommandLine;
 import picocli.CommandLine.Command;
 import picocli.CommandLine.Option;
 import picocli.CommandLine.Parameters;
 
-import hexlet.code.Differ;
-
 import java.util.concurrent.Callable;
 
-@Command(name = "gendiff", version = "gendiff 0.1",
+@Command(name = "gendiff",
+        mixinStandardHelpOptions = true,
+        version = "1.0",
         description = "Compares two configuration files and shows a difference.")
-public class App implements Callable<Integer> {
-    @Parameters(paramLabel = "filepath1", description = "path to first file")
-    private String firstFilePath;
+public final class App implements Callable<Integer> {
+    private static final int SUCCESS_EXIT_CODE = 0;
+    private static final int ERROR_EXIT_CODE = 1;
 
-    @Parameters(paramLabel = "filepath2", description = "path to second file")
-    private String secondFilePath;
+    @Option(names = {"-f", "--format"},
+            defaultValue = "stylish",
+            paramLabel = "format",
+            description = "output format [default: ${DEFAULT-VALUE}]")
+    private String outputFormat;
 
-    @Option(names = {"-f", "--format"}, paramLabel = "format", defaultValue = "stylish",
-            description = "output format [default: stylish]")
-    private String formatRequested;
+    @Parameters(paramLabel = "filepath1",
+            index = "0",
+            description = "path to first file")
+    private String filePath1;
 
-    @Option(names = {"-h", "--help"}, usageHelp = true, description = "Show this help message and exit.")
-    private boolean helpRequested = false;
-
-    @Option(names = {"-V", "--version"}, versionHelp = true, description = "Prints version information and exit.")
-    private boolean versionRequested = false;
+    @Parameters(paramLabel = "filepath2",
+            index = "1",
+            description = "path to second file")
+    private String filePath2;
 
     public static void main(String[] args) {
         int exitCode = new CommandLine(new App()).execute(args);
@@ -33,7 +37,14 @@ public class App implements Callable<Integer> {
 
     @Override
     public Integer call() {
-        System.out.println(Differ.generate(firstFilePath, secondFilePath, formatRequested));
-        return 0;
+        try {
+            String formattedDiff = Differ.generate(filePath1, filePath2, outputFormat);
+            System.out.println(formattedDiff);
+        } catch (Exception e) {
+            System.err.println(e.getMessage());
+            return ERROR_EXIT_CODE;
+        }
+
+        return SUCCESS_EXIT_CODE;
     }
 }
